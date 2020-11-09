8bbb5a82cf51f4819481aa1838a1ec45a78089e7
pragya0310 <73340960+pragya0310@users.noreply.github.com>
2020-11-09 19:49:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,14 +21,14 @@ if(wall.x-car.x<(car.width+wall.width)/2){
   car.velocityX=0;
   var deformation=0.5*weight*speed*speed/22500
 
-if(deformation>190){
-  car.shapeColor=('red')
+if(deformation>180){
+  car.shapeColor="red";
 }
-if(deformation<190 && deformation>100){
-car.shapeColor=('yellow');
+if(deformation<180 && deformation>100){
+car.shapeColor="yellow";
 }
 if(deformation<100){
-car.shapeColor=('green')
+car.shapeColor="green";
 }
 }
 
